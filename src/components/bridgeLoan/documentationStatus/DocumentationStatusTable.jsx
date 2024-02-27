9fddc190f9475899f4edebbb71aaafde76da1e28
Yetife <tifetreats21@gmail.com>
2024-02-27 17:29:09 +0100

    filter implementation

--- src/components/bridgeLoan/documentationStatus/DocumentationStatusTable.jsx
+++ src/components/bridgeLoan/documentationStatus/DocumentationStatusTable.jsx
@@ -154,7 +154,7 @@ export function TableData({data, no}) {
                     </svg>
                 </a>
                 <span onMouseLeave={handleBlurDropdown}
-                      className="absolute z-10 w-32  mt-2 shadow-md divide-y overflow-auto bg-white rounded-md cursor-pointer"
+                      className="absolute z-10 w-32 right--1 md:right-10  mt-2 shadow-md divide-y overflow-auto bg-white rounded-md cursor-pointer"
                       style={{display: showDropdown ? "block" : "none"}}>
                     <span
                         className="block px-4 w-full py-2 text-[14px] font-medium text-[#4A5D58] hover:bg-[#00C796]  hover:text-white"
